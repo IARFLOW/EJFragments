955074b39106e7899da13785c76345a96fb232a4
Ignacio Ampurdanes <ignaciopercusion@gmail.com>
2025-01-10 19:46:49 +0100

    Continuamos intenado ejecutar el color correcto en el toolbar de Lista de películas

--- app/src/main/java/com/example/ejfragments/vista/acticidades/MainActivity.java
+++ app/src/main/java/com/example/ejfragments/vista/acticidades/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -26,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements ListaPeliculas.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
